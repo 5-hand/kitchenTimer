8f1bfe64f4beabd5ea798b8d990e88f12e98137a
yusei <yusei5884@gmail.com>
2019-05-11 17:08:55 +0900

    新らしい関数を制作

--- countTimer/timer.js
+++ countTimer/timer.js
@@ -7,9 +7,26 @@ const reset = document.querySelector('.js-reset');
 let min = 0;
 let sec = 0;
 
-//引数にに1足して返す関数
+//引数に1足して返す関数
 function valuePlus(value){
     return value + 1;
+}
+
+//引数に1引いて返す関数
+function valueMinus(value){
+    return value - 1;
+}
+
+//timeを足した後にplaceに反映させる関数
+function timePlus(time,place){
+    time = valuePlus(time);
+    place.textContent = time;
+}
+
+//timeを引いた後にplaceに反映させる関数
+function timeMinus(time,place){
+    time = valueMinus(time);
+    place.textContent = time;
 }
 
 //分のボタンを押した時に数をカウントする記述
@@ -50,4 +67,11 @@ reset.addEventListener('click',function(){
     sec = 0;
     textMin.textContent = "00";
     textSec.textContent = "00";
+});
+
+//スタートボタンの挙動
+toggle.addEventListener('click',function(){
+    if(!(min == 0) || !(sec == 0)){
+        setInterval(timeMinus(sec,textSec),1000);
+    }
 });
