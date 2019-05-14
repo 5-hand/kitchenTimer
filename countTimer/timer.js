706fba4b076383a07585ee9bdaa0c7c348e7d2ca
yusei <yusei5884@gmail.com>
2019-05-15 01:10:12 +0900

    カウントダウンタイマー完成

--- countTimer/timer.js
+++ countTimer/timer.js
@@ -4,9 +4,11 @@ const minElem = document.querySelector('.js-min');
 const secElem = document.querySelector('.js-sec');
 const toggle = document.querySelector('.js-toggle');
 const reset = document.querySelector('.js-reset');
+let timerId;
 let min = 0;
 let sec = 0;
-let timer;
+let oldTime;
+let totalTime;
 
 //引数に1足して返す関数
 function valuePlus(value){
@@ -18,38 +20,26 @@ function valueMinus(value){
     return value - 1;
 }
 
-//timeを足した後にplaceに反映させる関数
-function timePlus(time,place){
-    time = valuePlus(time);
-    if(time < 10){
-    place.textContent = "0" + time;
+//minとsecのミリ秒を足して返す関数
+function getMS(minute,second){
+    let msMin = minute * 60000;
+    let msSec = second * 1000;
+    return msMin + msSec;
+}
+
+function pushZero(num ,place){
+    if(num < 10){
+        place.textContent = '0' + num;
     }else{
-    place.textContent = time;
+        place.textContent = num;
     }
 }
 
-//timeを引いた後にplaceに反映させる関数
-function timeMinus(time,place){   
-    timer = setInterval(function(){
-        if(time)
-            time = valueMinus(time);
-            if(time < 10){
-            place.textContent = "0" + time;
-            }else{
-            place.textContent = time;
-            }
-    },1000);
-}
-
 //分のボタンを押した時に数をカウントする記述
-minElem.addEventListener('click',function(){
+minElem.addEventListener('click',() =>{
     min = valuePlus(min);
     if(min < 100){
-        if(min < 10){
-        textMin.textContent = "0" + min;
-        }else{
-        textMin.textContent = min;
-        }
+        pushZero(min,textMin);
     }else{
     min =0;
     textMin.textContent = "0" + min;
@@ -57,32 +47,42 @@ minElem.addEventListener('click',function(){
 });
 
 //秒のボタンを押した時に数をカウントする記述
-secElem.addEventListener('click',function(){
+secElem.addEventListener('click',() => {
     sec = valuePlus(sec);
     if(sec < 60){
-        if(sec < 10){
-        textSec.textContent = "0" + sec;
-        }else{
-        textSec.textContent = sec;
-        }
+        pushZero(sec,textSec);
     }else{
     sec =0;
     textSec.textContent = "0" + sec;
     }
+    console.log(sec);
+});
+
+// スタートボタンを押した時の記述
+toggle.addEventListener('click',() =>{
+    oldTime = Date.now();
+    totalTime = getMS(min,sec);
+//1秒ごとに現在の時間と目的の時間を計算して表示
+    timerId = setInterval(() =>{
+    const currentTime = Date.now();
+    const diff = currentTime - oldTime;
+    const remainMSec = totalTime - diff;
+    const remainSec = Math.ceil((remainMSec /1000) % 60);
+    // msを分に変換
+    const remainMin = Math.trunc(remainMSec/60000);
+    pushZero(remainSec,textSec);
+    pushZero(remainMin,textMin);
+    if(remainMin == 0 && remainSec == 0){
+        clearInterval(timerId);
+    }
+},1000);
 });
 
 //リセットボタンを押した時の記述
-reset.addEventListener('click',function(){
-    clearInterval(timer);
+reset.addEventListener('click',() =>{
     min = 0;
     sec = 0;
     textMin.textContent = "00";
     textSec.textContent = "00";
-});
-
-//スタートボタンの挙動
-toggle.addEventListener('click',function(){
-    if(!(min == 0) || !(sec == 0)){
-        timeMinus(sec,textSec);
-    }
+    clearInterval(timerId);
 });
