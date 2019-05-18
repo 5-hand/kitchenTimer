da6b74751aae55633144915cce08d78a8126f507
yusei <yusei5884@gmail.com>
2019-05-18 19:05:58 +0900

    カウントダウンタイマー完成・ボタンのアウトライン消去

--- countTimer/timer.js
+++ countTimer/timer.js
@@ -10,6 +10,10 @@ let sec = 0;
 let oldTime;
 let totalTime;
 let startCnt = 0;
+let diff;
+let remainMsec;
+let remainMin;
+let remainSec;
 
 //引数に1足して返す関数
 function valuePlus(value){
@@ -37,15 +41,15 @@ function pushZero(num ,place){
     }
 }
 
+//buttonの有効・無効を切り替え
 function disableToggle(){
-    if(startCnt == 1){
+    if(startCnt > 0){
         minElem.disabled = true;
         secElem.disabled = true;
     }else{
         minElem.disabled = false;
         secElem.disabled = false;
-    }
-   
+    }   
 }
 
 //分のボタンを押した時に数をカウントする記述
@@ -57,7 +61,6 @@ minElem.addEventListener('click',() =>{
     min =0;
     pushZero(min,textMin);
     }
-    console.log(min);
 });
 
 //秒のボタンを押した時に数をカウントする記述
@@ -75,36 +78,72 @@ secElem.addEventListener('click',() => {
 toggle.addEventListener('click',() =>{
     startCnt++;
     disableToggle();
-    if(startCnt == 1){
-    oldTime = Date.now();
-    totalTime = getMS(min,sec);
-//1秒ごとに現在の時間と目的の時間を計算して表示
-    timerId = setInterval(() =>{
-    const currentTime = Date.now();
-    const diff = currentTime - oldTime;
-    const remainMSec = totalTime - diff;
-    const remainSec = Math.ceil((remainMSec /1000) % 60);
-    // msを分に変換
-    const remainMin = Math.trunc(remainMSec / 60000);
-    //６０秒を１分００秒で表示する記述
-    if(remainMin == 0 && remainSec == 0){
-        textSec.textContent = '00';
-        clearInterval(timerId);
-    }else if(remainSec % 60 === 0){
-        pushZero((remainMin + 1),textMin);
-        textSec.textContent = '00';
+    if(min == 0 && sec == 0){
+    // カウントアップタイマーの仕組み
+
     }else{
-        pushZero(remainSec,textSec);
-        pushZero(remainMin,textMin);
-    }
-},1000);
-}
+        //カウントダウンタイマーの仕組み
+        if(startCnt == 1){
+            oldTime = Date.now();
+            totalTime = getMS(min,sec);
+            //1秒ごとに現在の時間と目的の時間を計算して表示
+            timerId = setInterval(() =>{
+            const currentTime = Date.now();
+            diff = currentTime - oldTime;
+            remainMSec = totalTime - diff;
+            remainSec = Math.ceil((remainMSec /1000) % 60);
+            // msを分に変換
+            remainMin = Math.trunc(remainMSec / 60000);
+            //６０秒を１分００秒で表示する記述
+            if(remainMin == 0 && remainSec == 0){
+                textSec.textContent = '00';
+                clearInterval(timerId);
+            }else if(remainSec % 60 === 0){
+                pushZero((remainMin + 1),textMin);
+                textSec.textContent = '00';
+            }else{
+                pushZero(remainSec,textSec);
+                pushZero(remainMin,textMin);
+            }
+            },1000);
+    
+        }else if(startCnt == 2){
+            clearInterval(timerId);
+            startCnt++;
+        }else{
+            oldTime = Date.now();
+            totalTime = remainMSec;
+        //1秒ごとに現在の時間と目的の時間を計算して表示
+            timerId = setInterval(() =>{
+            const currentTime = Date.now();
+            diff = currentTime - oldTime;
+            remainMSec = totalTime - diff;
+            remainSec = Math.ceil((remainMSec /1000) % 60);
+            // msを分に変換
+            remainMin = Math.trunc(remainMSec / 60000);
+            //６０秒を１分００秒で表示する記述
+            if(remainMin == 0 && remainSec == 0){
+                textSec.textContent = '00';
+                clearInterval(timerId);
+            }else if(remainSec % 60 === 0){
+                pushZero((remainMin + 1),textMin);
+                textSec.textContent = '00';
+            }else{
+                pushZero(remainSec,textSec);
+                pushZero(remainMin,textMin);
+            }
+            },1000);
+            startCnt = 1;
+        }
+    } 
 });
 
 //リセットボタンを押した時の記述
 reset.addEventListener('click',() =>{
+    startCnt=0;
     min = 0;
     sec = 0;
+    disableToggle();
     pushZero(min,textMin);
     pushZero(sec,textSec);
     clearInterval(timerId);
